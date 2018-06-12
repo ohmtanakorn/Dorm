539f3ef66efb4d797f889594a257703d7341d8d5
Tanakorn Panthaikul <tanakornohm@tanakorns-macbook-pro.local>
2018-06-12 14:33:15 +0700

    Initial Commit

---
+++ Dorm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dorm
+//
+//  Created by Tanakorn Panthaikul on 12/6/2561 BE.
+//  Copyright © 2561 Tanakorn Panthaikul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
